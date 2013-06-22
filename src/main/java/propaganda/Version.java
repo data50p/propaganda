10218418dda7d534530002301b96ce42f084b86a
Lars Nordberg <lars@femtioprocent.se>
2013-06-22 20:32:45 +0200

    version 0.1.1.1

--- src/main/java/propaganda/Version.java
+++ src/main/java/propaganda/Version.java
@@ -2,6 +2,6 @@ package propaganda;
 	    public class Version {
 	    public static final String projectGroup = "com.femtioprocent";
 	    public static final String projectName = "propaganda";
-	    public static final String projectVersion = "0.1.1.1-SNAPSHOT";
-	    public static final String mavenBuildTimestamp = "20130621-1854";
+	    public static final String projectVersion = "0.1.1.1";
+	    public static final String mavenBuildTimestamp = "20130622-1111";
 	    }
