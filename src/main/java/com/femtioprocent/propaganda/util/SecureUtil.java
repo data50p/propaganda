e231b32c793a89a59459aecaa45e78d6823809f8
Lars Nordberg <lars@femtioprocent.se>
2013-10-08 23:44:48 +0200

    commence secure name

---
+++ src/main/java/com/femtioprocent/propaganda/util/SecureUtil.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.femtioprocent.propaganda.util;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author lars
+ */
+public class SecureUtil {
+
+    private static HashMap<String, String> m = new HashMap<String, String>();
+    
+    private static String salt = "" + System.getProperty("os.name") + System.nanoTime() + System.getProperty("user.name");
+
+    public static String getSecureName(String s) {
+        MessageDigest md = null;
+        try {
+            String us = s + ":" + salt;
+            md = MessageDigest.getInstance("SHA-1");
+            String ss = new String(md.digest(us.getBytes("utf-8")));
+            ss = Util.toAscii(ss);
+            m.put(ss, s);
+            return ss;
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        } catch (UnsupportedEncodingException ex) {
+            return null;
+        }
+    }
+
+    public static String lookupUnsecureName(String name) {
+        return m.get(name);
+    }
+}
