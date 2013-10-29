64e05c750e8623f0996b7c460d97cacf53d08614
Lars Nordberg <lars@femtioprocent.se>
2013-10-29 18:07:19 +0100

    use apache commons for SHA1

--- src/main/java/com/femtioprocent/propaganda/util/SecureUtil.java
+++ src/main/java/com/femtioprocent/propaganda/util/SecureUtil.java
@@ -4,12 +4,8 @@
  */
 package com.femtioprocent.propaganda.util;
 
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.apache.commons.codec.digest.DigestUtils;
 
 /**
  *
@@ -18,24 +14,19 @@ import java.util.logging.Logger;
 public class SecureUtil {
 
     private static HashMap<String, String> m = new HashMap<String, String>();
-    
+
     private static String salt = "" + System.getProperty("os.name") + System.nanoTime() + System.getProperty("user.name");
 
     public static String getSecureId(String s, String seasalt) {
-        MessageDigest md = null;
-        try {
-            String us = s + (seasalt == null || seasalt.length() == 0 ? "" : ":" + seasalt) + ":" + salt;
-            md = MessageDigest.getInstance("SHA-1");
-            byte[] d = md.digest(us.getBytes("utf-8"));
-            String ss = Util.toAscii(d);
-            System.err.println("SHA1: 2 " + ss.length());
-            m.put(ss, s);
-            return ss;
-        } catch (NoSuchAlgorithmException e) {
-            return null;
-        } catch (UnsupportedEncodingException ex) {
-            return null;
-        }
+        String us = s + (seasalt == null || seasalt.length() == 0 ? "" : ":" + seasalt) + ":" + salt;
+        String shaX = DigestUtils.shaHex(us);
+//        MessageDigest md = null;
+//            md = MessageDigest.getInstance("SHA-1");
+//            byte[] d = md.digest(us.getBytes("utf-8"));
+//            String ss = Util.toAscii(d);
+//            System.err.println("SHA1: 2 " + ss.length() + ' ' + ss + ' ' + shaX);
+        m.put(shaX, s);
+        return shaX;
     }
 
     public static String lookupUnsecureId(String id) {
