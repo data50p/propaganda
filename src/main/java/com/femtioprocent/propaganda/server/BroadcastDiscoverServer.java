08f02a1f3fd1133c3590aa263c386663117904f8
Lars Nordberg <lars@femtioprocent.se>
2014-02-02 18:16:25 +0100

    discover hostname

--- src/main/java/com/femtioprocent/propaganda/server/BroadcastDiscoverServer.java
+++ src/main/java/com/femtioprocent/propaganda/server/BroadcastDiscoverServer.java
@@ -6,6 +6,8 @@ package com.femtioprocent.propaganda.server;
 
 import com.femtioprocent.fpd.sundry.S;
 import com.femtioprocent.propaganda.Version;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.Inet4Address;
@@ -88,10 +90,22 @@ public class BroadcastDiscoverServer {
                                 throw new RuntimeException(e);
                             }
 
+                            Runtime r = Runtime.getRuntime();
+                            Process pr = r.exec("uname -n");
+                            pr.waitFor();
+                            BufferedReader b = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+                            String hostname = "";
+
+                            while ((hostname = b.readLine()) != null) {
+                                System.out.println(" --> " + hostname);
+                                break;
+                            }
+
                             String rpl = "{\"name\":\""
                                     + PropagandaServer.getDefaultServer().getName() + "\", \"version\":\""
                                     + Version.projectVersion + "\", \"buildtime\":\""
-                                    + Version.mavenBuildTimestamp + "\", \"hosts\":"
+                                    + Version.mavenBuildTimestamp + "\", \"host\":"
+                                    + hostname + "\", \"hosts\":"
                                     + hostList.toString().replace(" ", "") + ", \"port\":\""
                                     + PropagandaServer.getDefaultServer().serverPort
                                     + "\"}";
