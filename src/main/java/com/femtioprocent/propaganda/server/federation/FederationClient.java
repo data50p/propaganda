bb2491a7233bb0be08f80339ac75cb2e2948ee77
Lars Nordberg <lars@femtioprocent.se>
2016-02-18 21:01:14 +0100

    retry federation server connection

--- src/main/java/com/femtioprocent/propaganda/server/federation/FederationClient.java
+++ src/main/java/com/femtioprocent/propaganda/server/federation/FederationClient.java
@@ -20,69 +20,78 @@ public class FederationClient {
     private PrintWriter printWriter = null;
 
     public FederationClient(PropagandaServer server, String federation_join, int federation_port) throws IOException {
-        port = federation_port;
-        host = federation_join;
-        this.server = server;
-        startFederationClient();
-        System.err.println("new com.femtioprocent.propaganda.server.federation.FederationClient: " + federation_port);
+	port = federation_port;
+	host = federation_join;
+	this.server = server;
+	startFederationClient();
+	System.err.println("new com.femtioprocent.propaganda.server.federation.FederationClient: " + federation_port);
     }
 
     public String getId() {
-        return "FederationClient-" + this.hashCode();
+	return "FederationClient-" + this.hashCode();
     }
 
     private void startFederationClient() {
-        System.err.println("Start FedClient");
-        Thread sth = new Thread(() -> {
-            try {
-                socket = new Socket(host, port);
-                System.err.println("FedClient connected: " + socket);
-                socket.getInputStream();
-                BufferedReader rd = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
-                try {
-                    if (printWriter == null) {
-                        printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
-                    }
-                    System.err.println("sendToFed: " + printWriter);
-                } catch (IOException ex) {
-                    getLogger().severe("no-socket: " + socket.toString());
-                }
+	System.err.println("Start FedClient");
+	for(;;) {
+	    Thread sth = new Thread(() -> {
+		try {
+		    socket = new Socket(host, port);
+		    System.err.println("FedClient connected: " + socket);
+		    socket.getInputStream();
+		    BufferedReader rd = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+		    try {
+			if (printWriter == null) {
+			    printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
+			}
+			System.err.println("sendToFed: " + printWriter);
+		    } catch (IOException ex) {
+			getLogger().severe("no-socket: " + socket.toString());
+		    }
 
-                for (; ; ) {
-                    String sin = rd.readLine();
-                    System.err.println("FedClient got: " + sin);
-                    if (sin == null) {
-                        break;
-                    }
-                    Datagram datagram = new Datagram(sin);
-                    System.err.println("FedClient dispatch: " + datagram);
-                    server.dispatcher.dispatchMsg(null, datagram, printWriter);
-                }
-            } catch (Exception ex) {
-                System.err.println("FedClient ex: " + ex);
-            }
-        });
-        sth.start();
+		    for (;;) {
+			String sin = rd.readLine();
+			System.err.println("FedClient got: " + sin);
+			if (sin == null) {
+			    break;
+			}
+			Datagram datagram = new Datagram(sin);
+			System.err.println("FedClient dispatch: " + datagram);
+			server.dispatcher.dispatchMsg(null, datagram, printWriter);
+		    }
+		} catch (Exception ex) {
+		    System.err.println("FedClient ex: " + ex);
+		}
+	    });
+	    sth.start();
+	    
+	    try {
+		sth.join();
+		Thread.sleep(3000);
+	    } catch (Exception ex) {
+		    System.err.println("FedClient ex2: " + ex);		
+	    }
+	}
     }
 
     public void sendToFed(String s, PrintWriter avoid) {
-        System.err.println("sendToFed fso: " + socket);
-        if (socket == null) {
-            return;
-        }
+	System.err.println("sendToFed fso: " + socket);
+	if (socket == null) {
+	    return;
+	}
 
-        if (printWriter != avoid) {
-            if (printWriter != null) {
-                try {
-                    synchronized (printWriter) {
-                        printWriter.println(s);
-                        printWriter.flush();
-                        System.err.println("sendToFed send: " + s);
-                    }
-                } catch (Exception ex) {
-                    System.err.println("sendToFed send: " + ex);
-                }
-            }
-        }
+	if (printWriter != avoid) {
+	    if (printWriter != null) {
+		try {
+		    synchronized (printWriter) {
+			printWriter.println(s);
+			printWriter.flush();
+			System.err.println("sendToFed send: " + s);
+		    }
+		} catch (Exception ex) {
+		    System.err.println("sendToFed send: " + ex);
+		}
+	    }
+	}
     }
 }
